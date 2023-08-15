486eb7b9f51ecf1c7ed51988b36a3a166ef2731d
Pranara <100182812+Pranara@users.noreply.github.com>
2023-08-16 00:20:27 +0700

    Add files via upload

---
+++ Tugas Forum/asg_to_uploads.php
@@ -0,0 +1,186 @@
+<?php
+define("ROLE_A", "role_a");
+define("ROLE_B", "role_b");
+
+$host = 'localhost';  
+$username = 'root'; 
+$password = ''; 
+$dbname = 'dbforum';   
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+function hasRoleA($userRole) {
+    return $userRole === ROLE_A;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload Your File</title>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f5f5;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    .container {
+        background-color: #ffffff;
+        border-radius: 8px;
+        padding: 150px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        width: 400px;
+    }
+
+    h1 {
+        text-align: center;
+        color: #333;
+        margin-bottom: 20px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+        color: #555;
+    }
+
+    input[type="email"],
+    input[type="file"],
+    select {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 15px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    input[type="submit"] {
+        background-color: #007bff;
+        color: #fff;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        width: 100%;
+        transition: background-color 0.3s ease;
+    }
+
+    input[type="submit"]:hover {
+        background-color: #0056b3;
+    }
+
+    .error {
+        color: #ff5c5c;
+        font-size: 14px;
+        margin-top: 10px;
+    }
+
+    .success {
+        color: #2ecc71;
+        font-size: 14px;
+        margin-top: 10px;
+    }
+
+    select {
+        background-color: #f7f7f7;
+    }
+
+    input[type="file"] {
+        background-color: #f7f7f7;
+    }
+
+</style>
+
+
+</head>
+<body>
+    <div class="container">
+        <h1>Upload Your File</h1>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+            <label for="userEmail">Email:</label>
+            <input type="email" id="userEmail" name="userEmail" required>
+
+            <label for="userRole">Select your role:</label>
+            <select id="userRole" name="userRole" required>
+                <option value="<?php echo ROLE_A; ?>">Role A</option>
+                <option value="<?php echo ROLE_B; ?>">Role B</option>
+            </select>
+
+            <label for="uploadFile">Select a JPEG or PNG file:</label>
+            <input type="file" id="uploadFile" name="uploadFile" accept=".jpg, .jpeg, .png" required>
+
+            <input type="submit" value="Submit">
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["userEmail"]) && isset($_POST["userRole"])) {
+                $userEmail = $_POST["userEmail"];
+                $userRole = $_POST["userRole"];
+
+                if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+                    echo "<p class='error'>Invalid email format.</p>";
+                    exit;
+                   }
+
+                if (hasRoleA($userRole)) {
+                    if ($_FILES["uploadFile"]["error"] == UPLOAD_ERR_NO_FILE) {
+                        echo "<p class='error'>Please select a file.</p>";
+                        exit;
+                    }
+
+                    $allowedTypes = ['image/jpeg', 'image/png'];
+                    if (!in_array($_FILES["uploadFile"]["type"], $allowedTypes)) 
+                        {
+                        echo "<p class='error'>Only JPEG and PNG files are allowed.</p>";
+                        exit;
+                       }
+
+                    $targetDirectory = "dump_file/";
+                    $targetFile = $targetDirectory . basename($_FILES["uploadFile"]["name"]);
+
+                    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $targetFile)) {
+                        try {
+                            $stmt = $db->prepare("INSERT INTO uploaded_files (email, file_path) VALUES (:email, :file_path)");
+                            $stmt->bindParam(':email', $userEmail);
+                            $stmt->bindParam(':file_path', $targetFile);
+                            $stmt->execute();
+                            echo "<p class='success'>File uploaded successfully and data stored in the database.</p>";
+
+                        } catch (PDOException $e) 
+                            {
+                            echo "<p class='error'>Error storing data in the database: " . $e->getMessage() . "</p>";
+                            }
+                    } 
+                    else
+                        {
+                        echo "<p class='error'>Error uploading file.</p>";
+                        }
+                } 
+                else 
+                    {
+                    echo "<p class='error'>You don't have permission to upload files.</p>";
+                    }
+            } 
+            else 
+                {
+                echo "<p class='error'>Email or role field is missing.</p>";
+                }
+        }
+        ?>
+    </div>
+</body>
+</html>
---
+++ Tugas Forum/dbforum.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 09, 2023 at 09:51 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbforum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploaded_files`
+--
+
+CREATE TABLE `uploaded_files` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `upload_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `uploaded_files`
+--
+
+INSERT INTO `uploaded_files` (`id`, `email`, `file_path`, `upload_date`) VALUES
+(1, 'jorja.saputra132.js@gmail.com', 'uploads/instagram.png', '2023-08-09 19:45:20'),
+(2, 'jorja@dewaguard.com', 'uploads/instagram.png', '2023-08-09 19:45:25');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
